b0313b2136588a18674495b408a0ce168e29bd1c
yuxf <yuxf@unitechs.com>
2023-03-28 11:40:18 +0800

    replace path address

--- example_route_test.go
+++ example_route_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/Chise1/mux"
 )
 
 // This example demonstrates setting a regular expression matcher for
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/gorilla/mux
+module github.com/Chise1/mux
 
 go 1.12
